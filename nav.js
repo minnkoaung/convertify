0ae6e68c2f4151222c818ca3278df90762b58998
minnkoaung <minnkoaung@gmail.com>
2017-05-19 16:17:44 +0630

    add new nav & fix static img to static  sliding banner

---
+++ nav.js
@@ -0,0 +1,29 @@
+document.write(
+"<nav class='navbar navbar-default' role='navigation' style='z-index: 500 !important;'>"+
+                    "<div class='navbar-header'>"+
+                        "<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-ex1-collapse'>"+
+                            "<span class='sr-only'>Toggle navigation</span>"+
+                            "<span class='icon-bar'></span>"+
+                            "<span class='icon-bar'></span>"+
+                            "<span class='icon-bar'></span>"+
+                        "</button>"+
+                    "</div>"+
+                    "<div class='collapse navbar-collapse navbar-ex1-collapse'>"+
+                        "<ul class='nav navbar-nav animated fadeInUp'>"+
+                            "<li><a href='whats.html'>About</a></li>"+
+                            "<li><a href='foryou.html'>Convertify for you</a></li>"+
+                            "<!--<li><a href='#'>Business</a></li>-->"+
+                            "<li class='dropdown'>"+
+                                "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>Business <b class='caret'></b></a>"+
+                               " <ul class='dropdown-menu'>"+
+                                    "<li><a href='f&b.html'>F & B</a></li>"+
+                                   " <li><a href='projects.html'>Project Management</a></li>"+
+                                    "<li><a href='projects.html'>Business Contract</a></li>"+
+                                "</ul>"+
+                           " </li>"+
+                            "<li><a href='#'>Blog</a></li>"+
+                           " <li><a href='contact.html'>Contact Us</a></li>"+
+                       " </ul>"+
+                   " </div>"+
+               " </nav>"
+);
